1b18cdc073d9512abc5324276dbc08f02573e8a2
David Glick <david@glicksoftware.com>
2016-07-07 17:39:40 -0700

    continue to export connected places, not connection objects

--- pleiades/rdf/common.py
+++ pleiades/rdf/common.py
@@ -574,8 +574,7 @@ class PlaceGrapher(PleiadesGrapher):
                     log.exception("Couldn't wrap and graph %r", obj)
 
         # connects with
-        for f in (context.getConnections() +
-                  context.getConnections_from()):
+        for f in context.getConnectedPlaces():
             if self.wftool.getInfoFor(f, 'review_state') != 'published':
                 continue
             furl = f.absolute_url()
